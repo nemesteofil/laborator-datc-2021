4213120dfe3f0d933070c58969a32a1c69800b5e
nemesteofil <72358569+nemesteofil@users.noreply.github.com>
2021-11-14 22:05:41 +0200

    Add files via upload

---
+++ tema 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tema_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
